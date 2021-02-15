a176a5cd63df101de195c8cd3debb22fd3e7e1a8
tristan <westenskowtristan122@gmail.com>
2021-02-15 14:37:33 -0700

    final changes

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,193 @@
-// Assignment Code
+let specialCharacters = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+];
+
+let numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+let lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+let upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+function getPasswordOptions() {
+  let length = parseInt(
+    prompt("How many characters would you like your password to contain?")
+  );
+
+  if (isNaN(length) === true) {
+    alert("Password length must be provided as a number");
+    return;
+  }
+
+  if (length < 8) {
+    alert("Password length must be at least 8 characters");
+    return;
+  }
+
+  if (length > 128) {
+    alert("Password length must less than 129 characters");
+    return;
+  }
+
+  let hasSpecialCharacters = confirm(
+    "Click OK to confirm including special characters."
+  );
+
+  let hasNumericCharacters = confirm(
+    "Click OK to confirm including numeric characters."
+  );
+
+  let hasLowerCasedCharacters = confirm(
+    "Click OK to confirm including lowercase characters."
+  );
+
+  let hasUpperCasedCharacters = confirm(
+    "Click OK to confirm including uppercase characters."
+  );
+
+  if (
+    hasSpecialCharacters === false &&
+    hasNumericCharacters === false &&
+    hasLowerCasedCharacters === false &&
+    hasUpperCasedCharacters === false
+  ) {
+    alert("Must select at least one character type");
+    return;
+  }
+
+  let passwordOptions = {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCasedCharacters: hasLowerCasedCharacters,
+    hasUpperCasedCharacters: hasUpperCasedCharacters,
+  };
+
+  return passwordOptions;
+}
+
+function getRandom(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  let randElement = arr[randIndex];
+
+  return randElement;
+}
+
+function generatePassword() {
+  let options = getPasswordOptions();
+
+  let result = [];
+
+  let possibleCharacters = [];
+
+  let guaranteedCharacters = [];
+
+  if (options.hasSpecialCharacters) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+
+  if (options.hasNumericCharacters) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    guaranteedCharacters.push(getRandom(numericCharacters));
+  }
+
+  if (options.hasLowerCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteedCharacters.push(getRandom(lowerCasedCharacters));
+  }
+
+  if (options.hasUpperCasedCharacters) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteedCharacters.push(getRandom(upperCasedCharacters));
+  }
+
+  for (var i = 0; i < options.length; i++) {
+    let possibleCharacter = getRandom(possibleCharacters);
+
+    result.push(possibleCharacter);
+  }
+
+  for (var i = 0; i < guaranteedCharacters.length; i++) {
+    result[i] = guaranteedCharacters[i];
+  }
+
+  return result.join("");
+}
+
 let generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
